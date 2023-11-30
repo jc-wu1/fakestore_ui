5aec7b41c45b194dee09abd32c8342e5564939c9
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2023-11-30 11:15:37 +0700

    WIP search page

---
+++ lib/src/features/search/search_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../cart/cart_page.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SearchPageView();
+  }
+}
+
+class SearchPageView extends StatefulWidget {
+  const SearchPageView({super.key});
+
+  @override
+  State<SearchPageView> createState() => _SearchPageViewState();
+}
+
+class _SearchPageViewState extends State<SearchPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Search',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CartPage(),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.shopping_bag,
+              color: Theme.of(context).colorScheme.onPrimaryContainer,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
